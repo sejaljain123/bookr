5d9a73c5e8fcc6c44c07ee77471e3e5506a636c6
sejaljain123 <sejaljain2043@gmail.com>
2021-06-22 19:20:45 +0530

    changed img tags to next Image tags

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,11 +1,11 @@
 import styles from './Header.module.scss';
-import Image from 'next';
+import Image from 'next/image';
 import Hamburger from './Hamburger/Hamburger';
 function Header() {
   return (
     <nav className={styles.header}>
       <div className={styles.logo}>
-        <img src="/images/logo.png" alt="logo" />
+        <Image height="40px" width="50px" src="/images/logo.png" alt="logo" />
         <span>BOOK-R</span>
       </div>
       <ul className={styles.navbar}>
